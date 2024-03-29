6df80ad8105bcea0c46330dae36b25abbb21762f
eric nubor <nuboreric6@gmail.com>
2024-03-29 14:52:44 +0000

    lastest update on volunteers page

--- volunteer.php
+++ volunteer.php
@@ -13,7 +13,7 @@ include('./include/navbar.php');
                     <div class="col-12">
                         <div class="slider-content text-center">
                             <h6>Raise money for the poor and needy</h6>
-                            <h4>Empower a child with your donation</h4>
+                            <h2>Empower a child with your donation</h2>
                             <div class="slider-btn">
                                 <a href="#" class="btn black-btn">Donate<i class="fal fa-heart"></i></a>
                             </div>
@@ -30,148 +30,145 @@ include('./include/navbar.php');
 <!-- Volunteers pages -->
 <div class="site-section fund-raisers">
     <div class="container">
-      <div class="row mb-3 justify-content-center">
-        <div class="col-md-8 text-center">
-          <h2>Latest Donations</h2>
-          <p class="lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <p class="mb-5"><a href="#" class="link-underline">View All Donations</a></p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-6 col-lg-3 mb-5">
-          <div class="person-donate text-center">
-            <img src="images/person_1.jpg" alt="Image placeholder" class="img-fluid">
-            <div class="donate-info">
-              <h4>Jorge Smith</h4>
-              <span class="time d-block mb-3">Donated Just now</span>
-              <p>Donated <span class="text-success">$252</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Water Is Life. Clean Water In Urban Area</a></p>
+        <div class="row mb-3 justify-content-center">
+            <div class="col-md-8 text-center">
+                <h2>Latest Donations</h2>
+                <p class="lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="mb-5"><a href="#" class="link-underline">View All Donations</a></p>
             </div>
-          </div>    
         </div>
 
-        <div class="col-md-6 col-lg-3 mb-5">
-          <div class="person-donate text-center">
-            <img src="images/person_2.jpg" alt="Image placeholder" class="img-fluid">
-            <div class="donate-info">
-              <h4>Christine Charles</h4>
-              <span class="time d-block mb-3">Donated 1 hour ago</span>
-              <p>Donated <span class="text-success">$400</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Children Needs Education</a></p>
+        <div class="row">
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="person-donate text-center">
+                    <img src="images/person_1.jpg" alt="Image placeholder" class="img-fluid">
+                    <div class="donate-info">
+                        <h2>Jorge Smith</h2>
+                        <span class="time d-block mb-3">Donated Just now</span>
+                        <p>Donated <span class="text-success">$252</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Water Is Life. Clean Water In Urban Area</a></p>
+                    </div>
+                </div>
             </div>
-          </div>    
-        </div>
 
-        <div class="col-md-6 col-lg-3 mb-5">
-          <div class="person-donate text-center">
-            <img src="images/person_3.jpg" alt="Image placeholder" class="img-fluid">
-            <div class="donate-info">
-              <h4>Albert Sluyter</h4>
-              <span class="time d-block mb-3">Donated 4 hours ago</span>
-              <p>Donated <span class="text-success">$1,200</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Need Shelter for Children in Africa</a></p>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="person-donate text-center">
+                    <img src="images/person_2.jpg" alt="Image placeholder" class="img-fluid">
+                    <div class="donate-info">
+                        <h2>Christine Charles</h2>
+                        <span class="time d-block mb-3">Donated 1 hour ago</span>
+                        <p>Donated <span class="text-success">$400</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Children Needs Education</a></p>
+                    </div>
+                </div>
             </div>
-          </div>    
-        </div>
 
-        <div class="col-md-6 col-lg-3 mb-5">
-          <div class="person-donate text-center">
-            <img src="images/person_4.jpg" alt="Image placeholder" class="img-fluid">
-            <div class="donate-info">
-              <h4>Andrew Holloway</h4>
-              <span class="time d-block mb-3">Donated 9 hours ago</span>
-              <p>Donated <span class="text-success">$100</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Water Is Life. Clean Water In Urban Area</a></p>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="person-donate text-center">
+                    <img src="images/person_3.jpg" alt="Image placeholder" class="img-fluid">
+                    <div class="donate-info">
+                        <h2>Albert Sluyter</h2>
+                        <span class="time d-block mb-3">Donated 4 hours ago</span>
+                        <p>Donated <span class="text-success">$1,200</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Need Shelter for Children in Africa</a></p>
+                    </div>
+                </div>
             </div>
-          </div>    
-        </div>
 
-
-        <div class="col-md-6 col-lg-3 mb-5">
-          <div class="person-donate text-center">
-            <img src="images/person_1.jpg" alt="Image placeholder" class="img-fluid">
-            <div class="donate-info">
-              <h4>Jorge Smith</h4>
-              <span class="time d-block mb-3">Donated Just now</span>
-              <p>Donated <span class="text-success">$252</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Water Is Life. Clean Water In Urban Area</a></p>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="person-donate text-center">
+                    <img src="images/person_4.jpg" alt="Image placeholder" class="img-fluid">
+                    <div class="donate-info">
+                        <h2>Andrew Holloway</h2>
+                        <span class="time d-block mb-3">Donated 9 hours ago</span>
+                        <p>Donated <span class="text-success">$100</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Water Is Life. Clean Water In Urban Area</a></p>
+                    </div>
+                </div>
             </div>
-          </div>    
-        </div>
 
-        <div class="col-md-6 col-lg-3 mb-5">
-          <div class="person-donate text-center">
-            <img src="images/person_2.jpg" alt="Image placeholder" class="img-fluid">
-            <div class="donate-info">
-              <h4>Christine Charles</h4>
-              <span class="time d-block mb-3">Donated 1 hour ago</span>
-              <p>Donated <span class="text-success">$400</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Children Needs Education</a></p>
+
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="person-donate text-center">
+                    <img src="images/person_1.jpg" alt="Image placeholder" class="img-fluid">
+                    <div class="donate-info">
+                        <h2>Jorge Smith</h2>
+                        <span class="time d-block mb-3">Donated Just now</span>
+                        <p>Donated <span class="text-success">$252</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Water Is Life. Clean Water In Urban Area</a></p>
+                    </div>
+                </div>
             </div>
-          </div>    
-        </div>
 
-        <div class="col-md-6 col-lg-3 mb-5">
-          <div class="person-donate text-center">
-            <img src="images/person_3.jpg" alt="Image placeholder" class="img-fluid">
-            <div class="donate-info">
-              <h4>Albert Sluyter</h4>
-              <span class="time d-block mb-3">Donated 4 hours ago</span>
-              <p>Donated <span class="text-success">$1,200</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Need Shelter for Children in Africa</a></p>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="person-donate text-center">
+                    <img src="images/person_2.jpg" alt="Image placeholder" class="img-fluid">
+                    <div class="donate-info">
+                        <h2>Christine Charles</h2>
+                        <span class="time d-block mb-3">Donated 1 hour ago</span>
+                        <p>Donated <span class="text-success">$400</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Children Needs Education</a></p>
+                    </div>
+                </div>
             </div>
-          </div>    
-        </div>
 
-        <div class="col-md-6 col-lg-3 mb-5">
-          <div class="person-donate text-center">
-            <img src="images/person_4.jpg" alt="Image placeholder" class="img-fluid">
-            <div class="donate-info">
-              <h4>Andrew Holloway</h4>
-              <span class="time d-block mb-3">Donated 9 hours ago</span>
-              <p>Donated <span class="text-success">$100</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Water Is Life. Clean Water In Urban Area</a></p>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="person-donate text-center">
+                    <img src="images/person_3.jpg" alt="Image placeholder" class="img-fluid">
+                    <div class="donate-info">
+                        <h2>Albert Sluyter</h2>
+                        <span class="time d-block mb-3">Donated 4 hours ago</span>
+                        <p>Donated <span class="text-success">$1,200</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Need Shelter for Children in Africa</a></p>
+                    </div>
+                </div>
             </div>
-          </div>    
-        </div>
 
-      </div>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="person-donate text-center">
+                    <img src="images/person_4.jpg" alt="Image placeholder" class="img-fluid">
+                    <div class="donate-info">
+                        <h2>Andrew Holloway</h2>
+                        <span class="time d-block mb-3">Donated 9 hours ago</span>
+                        <p>Donated <span class="text-success">$100</span> <br> <em>for</em> <a href="#" class="link-underline fundraise-item">Water Is Life. Clean Water In Urban Area</a></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
-  </div> <!-- .section -->
+</div> <!-- .section -->
 
-  <div class="featured-section overlay-color-2" style="background-image: url('images/bg_1.jpg');">
-    
+<div class="featured-section overlay" style="background-image: url('images/bg_1.jpg');">
+    <!-- Content -->
     <div class="container">
-      <div class="row">
+        <div class="row">
 
-        <div class="col-md-6 mb-5 mb-md-0">
-          <img src="images/img_1.jpg" alt="Image placeholder" class="img-fluid">
+            <div class="col-md-6 mb-5 mb-md-0">
+                <img src="images/img_1.jpg" alt="Image placeholder" class="img-fluid">
+            </div>
+
+            <div class="col-md-6 pl-md-5">
+
+                <div class="form-volunteer">
+
+                    <h2>Be A Volunteer Today</h2>
+                    <form action="#" method="post">
+                        <div class="form-group">
+                            <!-- <label for="name">Name</label> -->
+                            <input type="text" class="form-control py-2" id="name" placeholder="Enter your name">
+                        </div>
+                        <div class="form-group">
+                            <!-- <label for="email">Email</label> -->
+                            <input type="text" class="form-control py-2" id="email" placeholder="Enter your email">
+                        </div>
+                        <div class="form-group">
+                            <!-- <label for="v_message">Email</label> -->
+                            <textarea name="v_message" id="" cols="30" rows="3" class="form-control py-2" placeholder="Write your message"></textarea>
+                            <!-- <input type="text" class="form-control py-2" id="email"> -->
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-white px-5 py-2" value="Send">
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div class="col-md-6 pl-md-5">
-
-          <div class="form-volunteer">
-            
-            <h2>Be A Volunteer Today</h2>
-            <form action="#" method="post">
-              <div class="form-group">
-                <!-- <label for="name">Name</label> -->
-                <input type="text" class="form-control py-2" id="name" placeholder="Enter your name">
-              </div>
-              <div class="form-group">
-                <!-- <label for="email">Email</label> -->
-                <input type="text" class="form-control py-2" id="email" placeholder="Enter your email">
-              </div>
-              <div class="form-group">
-                <!-- <label for="v_message">Email</label> -->
-                <textarea name="v_message" id="" cols="30" rows="3" class="form-control py-2" placeholder="Write your message"></textarea>
-                <!-- <input type="text" class="form-control py-2" id="email"> -->
-              </div>
-              <div class="form-group">
-                <input type="submit" class="btn btn-white px-5 py-2" value="Send">
-              </div>
-            </form>
-          </div>
-        </div>
-        
-      </div>
     </div>
-
-  </div> <!-- .featured-donate -->
-
+</div>
 
 
 
@@ -193,22 +190,23 @@ include('./include/navbar.php');
 
 
 
-<?php
-include('./include/footer.php');
-?>
 
-<!-- JS here -->
-<script src="./js/jquery-3.6.0.min.js"></script>
-<script src="./js/bootstrap.min.js"></script>
-<script src="./js/bootstrap-datepicker.min.js"></script>
-<script src="./js/isotope.pkgd.min.js"></script>
-<script src="./js/imagesloaded.pkgd.min.js"></script>
-<script src="./js/jquery.magnific-popup.min.js"></script>
-<script src="./js/jquery.odometer.min.js"></script>
-<script src="./js/jquery.easypiechart.min.js"></script>
-<script src="./js/jquery-ui.min.js"></script>
-<script src="./js/jquery.inview.min.js"></script>
-<script src="./js/jquery.appear.js"></script>
-<script src="./js/slick.min.js"></script>
-<script src="./js/wow.min.js"></script>
-<script src="./js/main.js"></script>
+        <?php
+        include('./include/footer.php');
+        ?>
+
+        <!-- JS here -->
+        <script src="./js/jquery-3.6.0.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+        <script src="./js/bootstrap-datepicker.min.js"></script>
+        <script src="./js/isotope.pkgd.min.js"></script>
+        <script src="./js/imagesloaded.pkgd.min.js"></script>
+        <script src="./js/jquery.magnific-popup.min.js"></script>
+        <script src="./js/jquery.odometer.min.js"></script>
+        <script src="./js/jquery.easypiechart.min.js"></script>
+        <script src="./js/jquery-ui.min.js"></script>
+        <script src="./js/jquery.inview.min.js"></script>
+        <script src="./js/jquery.appear.js"></script>
+        <script src="./js/slick.min.js"></script>
+        <script src="./js/wow.min.js"></script>
+        <script src="./js/main.js"></script>
