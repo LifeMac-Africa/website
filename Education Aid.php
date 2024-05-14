e07d7d8061275ea91db4cdc95f7b5528c6c65946
NK <destinynana566@gmail.com>
2024-05-13 20:07:48 -0500

    LIFE-MAC 05/05/13

--- Education Aid.php
+++ Education Aid.php
@@ -21,3 +21,237 @@ include("./include/navbar.php");
 </div>
 
 
+<div class="support-company-area section-padding">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            
+            <!-- Left Column -->
+            <div class="col-xl-6 col-lg-6 col-md-10">
+                <div class="support-location-img">
+                    <img class="imagee" src="./images/1500x500.jpeg" alt="" style="margin-bottom: 20px; border-radius:20px;">
+                </div>
+            </div>
+            
+            <!-- Right Column -->
+            <div class="col-xl-5 col-lg-6 col-md-10">
+                <div class="right-caption">
+                    
+                    <!-- Section Title -->
+                    <div class="section-tittlet">
+                        <h2>Welcome to EduLearn</h2>
+                    </div>
+                    
+                    <!-- Support Caption -->
+                    <div class="support-caption">
+                        <p class="pera-top">
+                            EduLearn is dedicated to providing accessible and quality education to learners of all ages. Our mission is to inspire a love for learning and empower individuals to reach their full potential through education. Join us in our commitment to lifelong learning and personal growth.
+                        </p>
+                        <!-- Learn More Button -->
+                        <a href="#" class="btn about-btn">Explore Courses</a>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div><br><br>
+
+
+<div class="countarea">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12 col-md-12">
+                <div class="count-down-wrapper">
+                    <div class="row justify-content-between">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="single-counter d-flex align-items-center">
+                                <div class="counter-img">
+                                    <img src="./images/students.svg" alt="">
+                                </div>
+                                <div class="captions">
+                                    <span class="counter">500 +</span>
+                                    <p class="color-green">Enrolled Students</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="single-counter d-flex align-items-center">
+                                <div class="counter-img">
+                                    <img src="./images/courses.svg" alt="">
+                                </div>
+                                <div class="captions">
+                                    <span class="counter">50</span>
+                                    <p class="color-green">Available Courses</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="single-counter d-flex align-items-center">
+                                <div class="counter-img">
+                                    <img src="./images/teachers.svg" alt="">
+                                </div>
+                                <div class="captions">
+                                    <span class="counter">100</span>
+                                    <p class="color-green">Qualified Teachers</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="single-counter d-flex align-items-center">
+                                <div class="counter-img">
+                                    <img src="./images/graduates.svg" alt="">
+                                </div>
+                                <div class="captions">
+                                    <span class="counter">1000+</span>
+                                    <p class="color-green">Graduates</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><br><br>
+
+
+<div class="our-services section-padding position-relative">
+    <div class="container">
+        <div class="row justify-content-sm-center">
+            <div class="col-xl-7 col-lg-8 col-md-11">
+                <div class="section-tittle text-center mb-70">
+                    <h2>Discover Our Offerings</h2>
+                    <p>At EduLearn, we offer a diverse range of educational programs and services designed to meet the needs of learners at every stage of their academic journey. From foundational courses to specialized training, we provide opportunities for growth and development in various fields.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-between">
+            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div class="single-services text-center mb-30">
+                <div class="services-icon">
+                        <img src="./images/seminar2.jpg" alt="">
+                    </div>
+                    <div class="services-cap">
+                        <h5><a href="#">Online Learning</a></h5>
+                        <p class="service-text">Access our courses anytime, anywhere with our convenient online learning platform. Learn at your own pace and engage with interactive content designed to enhance your learning experience.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div class="single-services text-center mb-30">
+                    <div class="services-icon">
+                        <img src="./images/seminar2.jpg" alt="">
+                    </div>
+                    <div class="services-cap">
+                        <h5><a href="#">Personalized Tutoring</a></h5>
+                        <p class="service-text">Receive individualized support and guidance from our experienced tutors. Whether you need assistance with homework, exam preparation, or mastering a new subject, our tutors are here to help you succeed.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div class="single-services text-center mb-30">
+                     <div class="services-icon">
+                        <img src="./images/seminar2.jpg" alt="">
+                    </div>
+                    <div class="services-cap">
+                        <h5><a href="#">Workshops & Seminars</a></h5>
+                        <p class="service-text">Participate in engaging workshops and seminars conducted by industry experts. Gain valuable insights and practical skills to enhance your knowledge and stay competitive in today's job market.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="containerb">
+    <div class="image-container">
+        <img src="./images/student.webp" alt="Students Studying">
+    </div>
+    <div class="text-container">
+        <h2>Start Your Learning Journey</h2>
+        <p>Embark on a journey of discovery and growth with EduLearn. Whether you're a student seeking academic support, a professional looking to expand your skillset, or an enthusiast eager to explore new interests, we have something for everyone. Join us and unlock your potential today!</p>
+        <a href="./courses.php" class="btn about-btn">Browse Courses</a>
+    </div>
+</div>
+
+
+<div class="join-us-area section-padding">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-xl-4 col-lg-6 col-md-10">
+                <div class="joing-details">
+                    <div class="section-tittle">
+                        <h2>Join Our Community</h2>
+                    </div>
+                    <p>Be part of a vibrant community of learners and educators at EduLearn. Connect with like-minded individuals, share knowledge and experiences, and collaborate on projects that inspire and challenge you. Together, we can make learning a truly enriching experience!</p>
+                    <a href="./register.php" class="btn about-btn">Join Now</a>
+                </div>
+            </div>
+            <div class="offset-xl-1 col-xl-4 col-lg-4 col-md-7 col-sm-7 d-flex">
+                <div class="joning-img flex-fill">
+                    <img src="images/uganda-DSC_2339.jpg" style="margin-bottom: 20px;" alt="">
+                </div>
+                
+                <div class="joning-img flex-fill">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include('./include/footer.php');
+?>
+
+		<!-- JS here -->
+        <script src="./js/jquery-3.6.0.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+        <script src="./js/bootstrap-datepicker.min.js"></script>
+        <script src="./js/isotope.pkgd.min.js"></script>
+        <script src="./js/imagesloaded.pkgd.min.js"></script>
+        <script src="./js/jquery.magnific-popup.min.js"></script>
+        <script src="./js/jquery.odometer.min.js"></script>
+        <script src="./js/jquery.easypiechart.min.js"></script>
+        <script src="./js/jquery-ui.min.js"></script>
+        <script src="./js/jquery.inview.min.js"></script>
+        <script src="./js/jquery.appear.js"></script>
+        <script src="./js/slick.min.js"></script>
+        <script src="./js/wow.min.js"></script>
+        <script src="./js/main.js"></script>
